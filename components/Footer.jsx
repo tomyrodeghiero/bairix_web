4b1b1ace4f241fdf7f5895d88ece815fe6bdfaf1
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2024-02-18 18:00:40 -0300

    Add link to APK

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,53 +6,57 @@ import { socials } from '../constants';
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
-const Footer = () => (
-  <motion.footer
-    variants={footerVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="footer-gradient" />
-    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Discover Bairix
-        </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-          <span className="font-normal text-[16px] text-white">
-            Download App
-          </span>
-        </button>
-      </div>
+const Footer = () => {
+  const apkUrl = 'https://expo.dev/artifacts/eas/qih8KC5BteQDZnof3H7Tdn.aab';
 
-      <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
-        <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            BAIRIX
+  return (
+    <motion.footer
+      variants={footerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className="footer-gradient" />
+      <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+        <div className="flex items-center justify-between flex-wrap gap-5">
+          <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+            Discover Bairix
           </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2024 Bairix. All rights reserved.
-          </p>
+          <a href={apkUrl} className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+            <span className="font-normal text-[16px] text-white">
+              Download App
+            </span>
+          </a>
+        </div>
 
-          <div className="flex gap-4">
-            {socials.map((social) => (
-              <a rel="noopener noreferrer" href={social.link} target="_blank">
-                <img
-                  key={social.name}
-                  src={social.url}
-                  alt={social.name}
-                  className="w-[48] h-[48px] object-contain cursor-pointer"
-                />
-              </a>
-            ))}
+        <div className="flex flex-col">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+          <div className="flex items-center justify-between flex-wrap gap-4">
+            <h4 className="font-extrabold text-[24px] text-white">
+              BAIRIX
+            </h4>
+            <p className="font-normal text-[14px] text-white opacity-50">
+              Copyright © 2024 Bairix. All rights reserved.
+            </p>
+
+            <div className="flex gap-4">
+              {socials.map((social) => (
+                <a rel="noopener noreferrer" href={social.link} target="_blank">
+                  <img
+                    key={social.name}
+                    src={social.url}
+                    alt={social.name}
+                    className="w-[48] h-[48px] object-contain cursor-pointer"
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </motion.footer>
-);
+    </motion.footer>
+  );
+};
 
 export default Footer;
